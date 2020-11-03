6a827258d86fa804db49011ccd6057e642587ef0
xiongge1993410a <692508978@qq.com>
2020-11-03 16:12:30 +0800

    控制用户的访问权限及优化

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,7 +14,7 @@ $(function () {
     var form = layui.form
     var layer = layui.layer
     // 通过 form.verify() 函数自定义校验规则
-    form.vsserify({
+    form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         // 校验两次密码是否一致的规则
@@ -30,40 +30,41 @@ $(function () {
         }
     })
 
-//监听注册表单的提交事件
-$(`#form_reg`).on(`submit`,function(e){
-    //阻止默认行为
-   e.preventDefault()
-   //发起ajax的post请求
-   var data={
-username:$(`#form_reg[name=username]`).val(),
-password:$(`#form_reg[name=password]`).val(),
-   }
-   $.post(`http://ajax.frontend.itheima.net/api/reguser`,data,function(res){
-       if(res.status!==0){
-           return layer.msg(res.message);
-       }
-      layer.msg(`注册成功!请登录`);
-   })
-})
-
-//监听登录表单的提交事件
-$(`#form_login`).submit(function(e){
-    e.preventDefault()
-    $.ajax({
-        method: "POST",  
-        url: "http://ajax.frontend.itheima.netl/api/login",  
-        data: $(this).serialize(),  
-        success: function (res) {  
-            if (res.status !== 0) {
-          return layer.msg('登录失败！')
+    //监听注册表单的提交事件
+    $(`#form_reg`).on(`submit`, function (e) {
+        //阻止默认行为
+        e.preventDefault()
+        //发起ajax的post请求
+        var data = {
+            username: $(`#form_reg[name=username]`).val(),
+            password: $(`#form_reg[name=password]`).val(),
         }
-layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-}
-})
-})
+        $.post(`http://ajax.frontend.itheima.net/api/reguser`, data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg(`注册成功!请登录`);
+        })
+    })
+
+    //监听登录表单的提交事件
+    $(`#form_login`).submit(function (e) {
+        console.log(1111);
+        e.preventDefault()
+        $.ajax({
+            method: "POST",
+            url: `http://ajax.frontend.itheima.net/api/login`,
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
 })
