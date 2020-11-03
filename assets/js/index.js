5652c9c20693755d3fec0174cd8addf273ed5232
xiongge1993410a <692508978@qq.com>
2020-11-03 15:07:29 +0800

    16统一为有权限的接口设置headers请求头

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,9 @@ function getUserInfo() {
         method: 'GET',
         url: '/my/userinfo',
         // headers 就是请求头配置对象
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
         success: function (res) {
             if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败！')
